b54262be6eb5d4774fae6f77faa1ba01654388f3
LucasCésar <lucasxcesar@gmail.com>
2024-04-27 02:37:22 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,35 @@
+
+
+let chave = "f79479d21bd3e622be8f1ee6fe85aa75"
+
+function colocarNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".img-previsao").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+
+}
+
+
+
+async function buscarCidade(cidade){
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
+    cidade + 
+    "&appid=" + 
+    chave + 
+    "&lang=pt_br" +
+    "&units=metric"
+    )
+    .then(resposta => resposta.json())
+
+    colocarNaTela(dados)
+}
+
+
+function cliqueiNoBotao(){
+   let cidade = document.querySelector(".input-cidade").value
+
+   buscarCidade(cidade)
+}
